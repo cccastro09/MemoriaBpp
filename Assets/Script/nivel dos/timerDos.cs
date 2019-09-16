9ee9c41109c2f5a31f930c6fcb6240e494c7c916
cccastro09 <castro_2003_007@hotmail.com>
2019-09-16 16:48:36 -0500

    correcciones pares agregacion de sonidos

---
+++ Assets/Script/nivel dos/timerDos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timerDos : MonoBehaviour
+{
+
+    public Transform Carga;
+    public Transform TextIndicador;
+    public Transform TextCargar;
+
+    [SerializeField]
+    private float Cantidad;
+
+    [SerializeField]
+    private float Velocidad;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cantidad < 300)
+        {
+            Cantidad += Velocidad * Time.deltaTime;
+            TextIndicador.GetComponent<Text>().text = ((int)Cantidad).ToString() + "";
+            TextCargar.gameObject.SetActive(true);
+        }
+        else
+        {
+            TextCargar.gameObject.SetActive(false);
+            TextIndicador.GetComponent<Text>().text = "fin";
+            SceneManager.LoadScene("perder");
+
+        }
+
+        Carga.GetComponent<Image>().fillAmount = Cantidad / 300;
+    }
+}
---
+++ Assets/Script/nivel tres/irTercerNivel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class irTercerNivel : MonoBehaviour
+{
+
+
+    /* public void llamarNivel()
+     {
+         SceneManager.LoadScene("SampleScene");
+     }*/
+    // Start is called before the first frame update
+
+
+
+    [System.Obsolete]
+    void OnMouseDown()
+    {
+        //Debug.Log("click");
+        Application.LoadLevel("TercerNivel");//paa cambiar de ecena revisar porque esta deprecado]\
+
+
+    }
+}
