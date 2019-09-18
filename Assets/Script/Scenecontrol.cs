15c1a5c61c3c9fe1cb248faa27aeae65c45277f6
cccastro09 <castro_2003_007@hotmail.com>
2019-09-18 13:20:02 -0500

    integracion  de elementos

--- Assets/Script/Scenecontrol.cs
+++ Assets/Script/Scenecontrol.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,7 +13,6 @@ public class Scenecontrol : MonoBehaviour
     public const float offsetX = 3f;
     public const float offeseY = 3f;
     
-
     public AudioClip carta, tema1;
     AudioSource sonido;
 
